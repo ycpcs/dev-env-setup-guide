dab9167eff3cbbaa0ac7eaf5d8b35ba17986d9af
James Moscola <jmoscola@ycp.edu>
2026-04-16 10:49:04 -0400

    updated copy button styling

--- js/copy_button.js
+++ js/copy_button.js
@@ -20,8 +20,8 @@ document.addEventListener("DOMContentLoaded", function () {
     btn.setAttribute("title", "Copy");
     btn.setAttribute("aria-label", "Copy");
 
-    wrapper.appendChild(btn);
     wrapper.appendChild(pre);
+    wrapper.appendChild(btn);
 
     btn.addEventListener("click", function () {
       var code = pre.querySelector("code")
